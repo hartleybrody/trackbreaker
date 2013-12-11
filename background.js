cd8cbbb535e7a39b09b47239e4aabdf09e834c37
Hartley Brody <hartley.brody@gmail.com>
2013-12-10 20:25:33 -0500

    remove console.log statments and better variable names

--- background.js
+++ background.js
@@ -5,24 +5,23 @@ function callback(info){
         method: info.method,
     };
 
-    var trackingDomains = [
+    var trackingUrls = [
         "//mailfoogae.appspot.com/t",           // streak
         "//app.getsignals.com/img.gif",         // signals (hubspot)
         "//app.yesware.com/t/",                 // yesware
     ];
 
     var cancelRequest = false;
-    _.each(trackingDomains, function(domain){
-        console.log("comparing " + domain + " with " + request.url);
-        if (request.url.indexOf(domain) != -1){
-            console.log("found a match");
+    _.each(trackingUrls, function(url){
+        if (request.url.indexOf(url) != -1){
             cancelRequest = true;
         }
     });
     return {"cancel": cancelRequest};
 }
 
-var filter = {
+// only examine requests from known tracking domains
+var trackingDomains = {
     "urls": [
         "*://*.appspot.com/*",
         "*://*.getsignals.com/*",
@@ -31,4 +30,4 @@ var filter = {
 };
 
 // set the listener on the right webRequest event
-chrome.webRequest.onBeforeRequest.addListener(callback, filter, ["blocking"]);
+chrome.webRequest.onBeforeRequest.addListener(callback, trackingDomains, ["blocking"]);
